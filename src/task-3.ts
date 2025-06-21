606adb4859443b6de1228e455f1d4792e92eb7e2
Kateryna <tyutyunnikova.katia@gmail.com>
2025-06-21 11:19:24 +0300

    task3

--- src/task-3.ts
+++ src/task-3.ts
@@ -1,3 +1,8 @@
+interface Product {
+  id: number;
+  title: string;
+}
+
 const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
@@ -6,10 +11,7 @@ const products: Product[] = [
   { id: 1, title: "Phone" },
   { id: 2, title: "Laptop" },
 ];
-interface Product {
-  id: number;
-  title: string;
-}
+
 console.log(`Usernames: ${JSON.stringify(usernames)}`);
 console.log(`Ratings: ${JSON.stringify(ratings)}`);
 console.log(`Products: ${JSON.stringify(products)}`);
